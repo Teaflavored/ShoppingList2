5a600be6b403a3b8d5f4421e730e37bc0ee7af6d
Auster Chen <Auster.s.chen@gmail.com>
2014-12-11 10:07:50 -0800

    added routes/controller for texts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "static_pages#home"
   namespace :api, defaults: { format: :json } do
     resources :lists, only: [:index, :create, :show]
+    get "/texts", to: "texts#send_text"
     resources :items, only: [:index, :create]
   end
 end
