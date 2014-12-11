9eb432657166820757ef76dedf238ddf8275ec3f
Auster Chen <Auster.s.chen@gmail.com>
2014-12-10 19:55:55 -0800

    added static page and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "static_pages#home"
   namespace :api, defaults: { format: :json } do
     resources :lists, only: [:index, :create, :show]
     resources :items, only: [:index, :create]
